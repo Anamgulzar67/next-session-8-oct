8ff3fcacd49b93360e7dd77edfc01254e39dbe5d
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-11-20 20:10:56 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Button from "@/components/Button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-5">
-      <h1>Hello from Nexjs</h1>
+      <h1>Frontend Developer</h1>
       <h1 className="font-bold text-6xl text-red-500">
-        Hello Next Js 
+        Anam Gulzar 
       </h1>
 
       <p>
